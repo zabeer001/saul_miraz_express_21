3424c2e7ae34c1a31da17a2e65885515ee3ced1f
zabeer001 <binzabirtareq@gmail.com>
2025-07-21 17:35:29 +0600

    udpated

--- models/product.model.js
+++ models/product.model.js
@@ -2,73 +2,70 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        default: null,
-    },
-    image: {
-        type: String,
-        default: null, // single main image (optional)
-    },
-    media: {
-        type: [
-            {
-                file_path: { type: String, required: true },
-                alt: { type: String, default: '' },
-                order: { type: Number, default: 0 },
-            }
-        ],
-        default: [],
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    category_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Category',
-    },
-    status: {
-        type: String,
-        required: true,
-    },
-    arrival_status: {
-        type: String,
-        default: 'regular',
-    },
-    cost_price: {
-        type: Number,
-        required: true,
-    },
-    stock_quantity: {
-        type: Number,
-        default: 1,
-    },
-    arrival_status: {
-        type: String,
-        default: 'regular',
-    },
-    rating: {
-        type: Number,
-        default: 'regular',
-    },
-    total_review: {
-        type: Number,
-        default: 'regular',
-    },
-    sales: {
-        type: Number,
-        default: 0,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: null,
+  },
+  image: {
+    type: String,
+    default: null, // single main image (optional)
+  },
+  media: {
+    type: [
+      {
+        file_path: { type: String, required: true },
+        alt: { type: String, default: '' },
+        order: { type: Number, default: 0 },
+      }
+    ],
+    default: [],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Category',
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  arrival_status: {
+    type: String,
+    default: 'regular',
+  },
+  cost_price: {
+    type: Number,
+    required: true,
+  },
+  stock_quantity: {
+    type: Number,
+    default: 1,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  total_review: {
+    type: Number,
+    default: 0,
+  },
+  sales: {
+    type: Number,
+    default: 0,
+  }
 }, { timestamps: true });
 
 // Apply pagination plugin
 productSchema.plugin(mongoosePaginate);
 
 const Product = mongoose.model('Product', productSchema);
+
 export default Product;
